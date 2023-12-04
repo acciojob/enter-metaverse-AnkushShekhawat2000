29222bb6aa7e67cd79b2e7c44887bd83c82d7336
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-05 00:15:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let para=document.getElementById('status');
+let enterBtn = document.getElementById('enter-btn');
   enterBtn.addEventListener("click",function(){
    let heading=document.createElement("h1");
    heading.innerText="Entered Metaverse";
