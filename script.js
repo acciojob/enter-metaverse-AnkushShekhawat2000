a65a3dbb9a90cbf0dbf500f6b129b9e6802c1d37
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-05 00:26:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-// script.js
 
-let para = document.getElementById('status');
-let enterBtn = document.getElementById('enter-btn');
-
-enterBtn.addEventListener("click", function () {
-  let heading = document.createElement("h1");
-  heading.innerText = "Entered Metaverse";
-  para.innerText = "";
+document.querySelector('#enter-btn').addEventListener('click', function () {
+  let para = document.querySelector('#status');
+  let heading = document.createElement('h1');
+  heading.innerText = 'Entered Metaverse';
+  para.innerText = '';
   para.appendChild(heading);
 });
