6716276f219087937c53f454b502ea37cfc369f7
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-05 00:12:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-//your JS code here. If required.
-let  para = document.getElementById("status");
-
-const Button = document.getElementById("enter-btn");
-Button.addEventListener("click", function()){
-	let heading = document.createElement("h1");
-     heading.innerText = "Entered the Metaverse";
-	para.innerText ="";
-	para.appendChild(heading);
-}
+let para=document.getElementById('status');
+  enterBtn.addEventListener("click",function(){
+   let heading=document.createElement("h1");
+   heading.innerText="Entered Metaverse";
+   para.innerText="";
+   para.appendChild(heading);
+  })
