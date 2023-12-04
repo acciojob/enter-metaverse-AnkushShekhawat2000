cb8cbbc4d8612e82b09d55e1451e83e138b2e740
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-05 00:03:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let  para = document.getElementById("status");
+
+const Button = document.getElementById("enter-btn");
+Button.addEventListener("click", function()){
+	let heading = document.createElement("h1");
+
+	para.innerText ="";
+	para.appendChild(heading);
+}
